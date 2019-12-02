d03c4686ce662dcea97f8a8e0c439da2b46bb75b
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2019-12-02 12:20:58 +0100

    save2

---
+++ page/formulaire_inscription.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+  </head>
+  <body>
+    <form class="" action="#" method="post">
+      <div class="">
+        <label for="">nom</label>
+        <input type="text" name="nom" value="">
+      </div>
+      <div class="">
+        <label for="">prenom</label>
+        <input type="text" name="nom" value="">
+      </div>
+      <div class="">
+        <label for="">age</label>
+        <input type="text" name="nom" value="">
+      </div>
+      <div class="">
+        <label for="">mail</label>
+        <input type="text" name="nom" value="">
+      </div>
+      <div class="">
+        <label for="">adresse</label>
+        <input type="text" name="nom" value="">
+      </div>
+      <div class="">
+        <label for="">classe</label>
+        <select class="" name="classe">
+          <option value="STI2D">bac STI2D</option>
+          <option value="TU">bac pro TU</option>
+          <option value="MEI">bac pro MEI</option>
+          <option value="SN">bac pro SN</option>
+          <option value="CPRP">BTS CPRP</option>
+          <option value="SLAM">BTS SIO option SLAM</option>
+          <option value="SISR">BTS SIO option SISR</option>
+        </select>
+      </div>
+      <div class="">
+        <label for="">mot de passe</label>
+        <input type="password" name="mdp" value="">
+      </div>
+      <div class="">
+        <label for="">statut</label>
+        <select class="" name="status">
+          <option value="parent">Parent</option>
+          <option value="eleve">Eleve</option>
+        </select>
+      </div>
+    </form>
+  </body>
+</html>
