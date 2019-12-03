e9cbb5f88557a0026f32ebd3cb1ded973ca7b0f4
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2019-12-03 08:21:49 +0100

    save7

--- page/navbar.php
+++ page/navbar.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <section>
   <div class="">
-
+    <a href="#"></a>
   </div>
   <div class="">
-
+    <img href="#" src="" alt="">
   </div>
   <div class="">
-
+    <a href="#">Formation</a>
   </div>
   <div class="">
-
+    <a href="#">Relation Entrprise</a>
+  </div>
+  <div class="">
+    <a href="#">Vie dans l'établissement</a>
+  </div>
+  <div class="">
+    <a href="#">Voyage</a>
+  </div>
+  <div class="">
+    <a href="#">Connexion</a>
   </div>
 </section>
