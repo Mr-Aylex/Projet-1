1225de07ddd028bd525902411b500b6ef9675664
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2019-12-03 10:10:11 +0100

    save11

--- page/navbar.php
+++ page/navbar.php
@@ -1,47 +1,46 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="..\CSS\navbar_style.css">
 <section>
   <section>
-    <div class="">
-      <a href="#"></a>
-    </div>
-    <div class="">
+    <div class="boutton">
+      <p>Icon</p>
       <img href="#" src="" alt="">
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Formation</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Relation Entrprise</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Vie dans l'établissement</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Voyage</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Connexion</a>
     </div>
   </section>
   <section>
-    <div class="">
+    <div class="boutton">
       <a href="#">Pronote</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Contacte</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">FaceBook</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Dates</a>
     </div>
-    <div class="">
+    <div class="boutton">
       <a href="#">Documents</a>
     </div>
   </section>
   <section>
-    <div class="">
+    <div class="carroussel">
       <h3>Carrousel</h3>
     </div>
   </section>
--- page/page_principale.php
+++ page/page_principale.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="..\CSS\page_principal.css">
   </head>
   <body>
     <?php include "navbar.php" ?>
