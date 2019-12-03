5f36c8b436345fc3b961f9acaf110a68b7a4730a
CARMONE Alexandre <carmonealexandre@hotmail.fr>
2019-12-03 11:39:37 +0100

    save13

--- page/navbar.php
+++ page/navbar.php
@@ -10,7 +10,7 @@
           <a class="lien" href="#">Formation</a>
       </li>
       <li class="item"class="boutton">
-          <a class="lien" href="#">Entrprise</a>
+          <a class="lien" href="#">Entreprise</a>
       </li>
       <li class="item"class="boutton">
           <a class="lien" href="#">Vie</a>
@@ -19,7 +19,7 @@
           <a class="lien" href="#">Voyage</a>
       </li>
       <li class="item"class="boutton">
-          <a class="lien" href="#">Connexion</a>
+          <a class="lien" href="formulaire_connexion.php">Connexion</a>
       </li>
     </ul>
 
@@ -29,10 +29,10 @@
       <a class="lien" href="#">Pronote</a>
     </div>
     <div class="boutton">
-      <a class="lien" href="#">Contacte</a>
+      <a class="lien" href="#">Contact</a>
     </div>
     <div class="boutton">
-      <a class="lien" href="#">FaceBook</a>
+      <a class="lien" href="#">Facebook</a>
     </div>
     <div class="boutton">
       <a class="lien" href="#">Dates</a>
@@ -43,7 +43,32 @@
   </section>
   <section>
     <div class="carroussel">
-      
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="..." alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   </section>
-</section>
+</section class="nav">
+<link href="../CSS/bootstrap.css" rel="stylesheet" type="text/css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script type="../js/bootstrap.js"></script>
