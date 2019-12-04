d353cf13fa8357c35189cd7b3a183a294319e7a4
Sullivan SEXTIUS <SEXTIUS_Su@lprs.local>
2019-12-04 12:31:39 +0100

    save24

--- page/page_principale.php
+++ page/page_principale.php
@@ -5,32 +5,66 @@
     <title></title>
     <link rel="stylesheet" href="..\CSS\page_principal.css">
   </head>
-  <body class="font">
+  <body class="body">
     <?php include "navbar.php" ?>
     <link href="bootstrap-4.3.1-dist/css/bootstrap.css" rel="stylesheet" type="text/css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
       <script type="js/bootstrap.js"></script>
-      <section class="navbarB">
-        <div class="boutton">
-          <a class="lien" href="#">Pronote</a>
-        </div>
-        <div class="boutton">
-          <a class="lien" href="#">Contact</a>
-        </div>
-        <div class="boutton">
-          <a class="lien" href="#">Facebook</a>
-        </div>
-        <div class="boutton">
-          <a class="lien" href="#">Dates</a>
-        </div>
-        <div class="boutton">
-          <a class="lien" href="#">Documents</a>
-        </div>
+          <section class="navbarC">
+
+        <ul class="navigation">
+          <li>
+            <div class="boutton">
+
+              <div class="div">
+                  <a href="#" class="lien1" title="aller à la section 1">Pronote</a>
+              </div>
+
+              <div/>
+              </li>
+
+              <li>
+                <div class="boutton">
+                  <div class="div1">
+                    <a href="#" class="lien1" title="aller à la section 1">Contact</a>
+                  </div>
+
+                  <div/>
+                  </li>
+                  <li>
+                    <div class="boutton">
+                      <div class="div2">
+                      <a href="#" class="lien1" title="aller à la section 1">Facebook</a>
+                        </div>
+                      <div/>
+                      </li>
+                      <li>
+                        <div class="boutton">
+                          <div class="div3">
+                              <a href="#" class="lien1" title="aller à la section 1">Dates</a>
+                          </div>
+
+                          <div/>
+                          </li>
+                          <li>
+                            <div class="boutton">
+                              <div class="div4">
+                                    <a href="#" class="lien1" title="aller à la section 1">Documents</a>
+                              </div>
+
+                              <div/>
+                              </li>
+</ul>
+
+
+
       </section>
 
-      <div class="carroussel">
+      <div class="carroussel1">
+
+
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
@@ -52,7 +86,7 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
-      </div>
+          </div>
 
   </body>
 </html>
